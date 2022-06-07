ef11e5971c40c105f057830e1b2f93755e27c09b
DexNick4501 <idesignerdisk2@yandex.r>
2022-06-07 10:15:01 +0500

    layout main page

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::get('/products/add', function () {
+    return view('products.add');
+})->name('products.add');
